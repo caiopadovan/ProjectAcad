b89f25086836ef11a022fb433bc1ce4ed7eb8417
Caio Sverzut Padovan <104374481+caiopadovan@users.noreply.github.com>
2026-07-24 18:15:08 -0300

    Adding JWT authorization

--- ProjectAcad/settings.py
+++ ProjectAcad/settings.py
@@ -45,6 +45,12 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 CORS_ALLOWED_ORIGINS = [
